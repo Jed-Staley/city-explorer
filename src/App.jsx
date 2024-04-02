e37185c44c4a5bbde765007de9615c86689583d3
Jed Staley <JNStaley1@gmail.com>
2024-04-02 10:22:43 -0400

    Made basic functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,62 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {When} from 'react-if';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const APIkey = import.meta.env.VITE_API_KEY;
 
+function App() {
+  
+  const [userInput, setUserInput] = useState('');
+  const [location, setLocation] = useState({});
+
+  const updateUserInput = newStr => setUserInput(newStr.target.value);
+  const handleSubmit = cityStr => {
+    cityStr.preventDefault();
+    fetchLocation();
+  }
+
+  async function fetchLocation() {
+
+    try {
+      let jsonData;
+
+      try {
+        console.log(`https://us1.locationiq.com/v1/search?key=${APIkey}&q=${userInput}&format=json`)
+        jsonData = await fetch(`https://us1.locationiq.com/v1/search?key=${APIkey}&q=${userInput}&format=json`);
+      } catch(error) {
+        console.error('Error: Failed to contact server', error);
+      }
+
+      let parsedData = await jsonData.json();
+      setLocation(parsedData[0]);
+
+    } catch(error) {
+      console.error('Error: Unable to parse received data', error);
+    }
+  }
+  
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <form onSubmit={handleSubmit}>
+        <input placeholder='Explore!' onChange={updateUserInput} />
+      </form>
+
+      {
+        location.display_name ?
+          <section>
+            <h4>Location Information For: {location.display_name}</h4>
+          </section>
+        : null
+      }
+
+      <When condition={location.lat && location.lon}>
+        <section>
+          <h5>Latitude: {location.lat} Longitude: {location.lon}</h5>
+          <img src={`https://maps.locationiq.com/v3/staticmap?key=${APIkey}&center=${location.lat},${location.lon}&size=500x440&zoom=10`} />
+        </section>
+      </When>
+
     </>
   )
 }
