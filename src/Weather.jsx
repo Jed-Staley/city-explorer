f35d41fa421dd8ea1723f73bc4b593fb4fb0c224
Jed Staley <JNStaley1@gmail.com>
2024-04-04 23:00:12 -0400

    Styled weather data

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,16 +1,44 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
+
 function Weather({ weatherData }) {
+  function WeatherDay({ day }) {
+    const dateParts = day.date.split('-');
+    const year = parseInt(dateParts[0]);
+    const month = parseInt(dateParts[1]) - 1;
+    const newDay = parseInt(dateParts[2]);
+
+    const dateObject = new Date(year, month, newDay);
+    const dayOfWeekIndex = dateObject.getDay();
+  
+    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  
+    const finalDay = daysOfWeek[dayOfWeekIndex];
+
+    return (
+      <Card>
+        <Card.Header>{finalDay}</Card.Header>
+        <Card.Body>
+          <Card.Text>{day.description}</Card.Text>
+        </Card.Body>  
+        <Card.Footer>
+          <small>{day.date}</small>
+        </Card.Footer>
+      </Card> 
+    );
+  }
+
   return (
-    <>
-      <section>
-        <h2>Weather Data</h2>
-        {weatherData && weatherData.map(item => (
-          <p key={item.date}>
-            {item.date}: {item.description}
-          </p>
+    <section className="card-container1">
+      <h2>Forecast</h2>
+      <CardGroup>
+        {weatherData.map((day) => (
+          <WeatherDay day={day} key={day.date} />
         ))}
-      </section>
-    </>
-  )
+      </CardGroup>
+    </section>
+  );
 }
 
 export default Weather
